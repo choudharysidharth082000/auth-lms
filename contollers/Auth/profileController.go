81135bd9fe5a75045dfabab2e4ddbd0454da5dc8
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2022-11-06 23:42:10 +0530

    Profile Routes completed

--- contollers/Auth/profileController.go
+++ contollers/Auth/profileController.go
@@ -2,6 +2,7 @@ package Auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -25,4 +26,22 @@ func UpdateProfileController(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(commons.Response{Status: 400,Message: "Profile is not updated"})
 	}
+}
+
+//getting all the profiles from the user
+func GetAllProfilesController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	profiles := GetAllProfiles()
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(commons.Response{Status:200,Message:"All the profiles",Data:profiles});
+	fmt.Println("Profile Router is Called");
+}
+//getting the profile by id
+func GetProfileController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	profile := GetProfile(params["id"])
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(commons.Response{Status:200,Message:"Profile",Data:profile});
+	fmt.Println("Profile Router is Called");
 }
--- main.go
+++ main.go
@@ -11,9 +11,14 @@ import (
 
 func main() {
 	newRouter := mux.NewRouter()
-	newRouter.HandleFunc("/v1/api/login", Auth.LoginController).Methods("GET")
-	newRouter.HandleFunc("/v1/api/signup", Auth.SignupController).Methods("POST")
+	newRouter.HandleFunc("/v1/api/auth/login", Auth.LoginController).Methods("GET")
+	newRouter.HandleFunc("/v1/api/auth/signup", Auth.SignupController).Methods("POST")
 	newRouter.HandleFunc("/test", Auth.TestController).Methods("GET")
+
+	//profile routes
+	newRouter.HandleFunc("/v1/api/profile/getProfile/{id}", Auth.GetProfileController).Methods("GET")
+	newRouter.HandleFunc("/v1/api/profile/updateProfile", Auth.UpdateProfileController).Methods("PUT")
+	newRouter.HandleFunc("/v1/api/profile/getProfile", Auth.GetAllProfilesController).Methods("GET")
 	//serve the server on port 4040
 	fmt.Println("Server is running on port 4040")
 	log.Fatal(http.ListenAndServe(":4040", newRouter))	
